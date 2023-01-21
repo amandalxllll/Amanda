d5428ef18243d4e8ea680e77f414bc7423eabeb5
amandalxllll <xliu7@babson.edu>
2023-01-20 21:03:15 -0500

    Complete HW1.py

---
+++ HW1.py
@@ -0,0 +1,18 @@
+# %%
+#How many seconds are there in 42 minutes 42 second?
+print('Question 1 answer:')
+print(42*60+42)
+
+#How many miles are there in 10 kilometers?
+print('Question 2 answer:')
+print(10*1.61)
+
+#If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)? What is your average speed in miles per hour?
+print("average pace per mile in minutes")
+print(2562/16.1/60)
+print("average pace per mile in minute")
+print(2562/16.1)
+print("speed in miles per hour")
+print(16.1/2562*60*60)
+
+
