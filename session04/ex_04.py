dc031d737d8d5c625467c48ab6493f519e6d87a0
amandalxllll <xliu7@babson.edu>
2023-01-27 12:04:29 -0500

    finished

---
+++ session04/ex_04.py
@@ -0,0 +1,9 @@
+
+a = int (input ("please type in the first number >>"))
+b = int (input ("please type in the second number >>"))
+
+print(f"{a+b:>8}")
+print(f"{a-b:>8}")
+print(f"{a*b:>8}")
+print(f"{a/b:>8}")
+print(f"{a**b:>8}")
