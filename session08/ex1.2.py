477d67b2dde5532806840f95bca185b262485f6c
amandalxllll <xliu7@babson.edu>
2023-02-13 20:56:19 -0500

    Update ex1.2.py

--- session08/ex1.2.py
+++ session08/ex1.2.py
@@ -1,31 +1,36 @@
-#Exercuse 2
-def calculate_bmi (weight, height):
+# Exercuse 2
+def calculate_bmi(weight, height):
     """
     return BMI value
     """
-    bmi = weight/(height**2)
+    bmi = weight / (height**2)
+    return bmi
+testcode = calculate_bmi(60,1.71)
+print(testcode)
 
-    return calculate_bmi (60,1.71)
 
-#Exercise 2.1
-weight=float(input("please input your weight (in kg)>>"))
-height=float (input("please input your height (in m)>>"))
 
-bmi = weight /(height**2)
+# Exercise 2.1
+weight = float(input("please input your weight (in kg)>>"))
+height = float(input("please input your height (in m)>>"))
 
-def calculate_bmi(weight,height):
+bmi = weight / (height**2)
+
+
+def calculate_bmi1(weight, height):
     """
     return BMI value and category
     """
-    bmi = weight /(height**2)
+    bmi = weight / (height**2)
     if bmi <= 18.5:
         return "Underweight"
-    elif 18.5<bmi<24.9:
+    elif 18.5 < bmi < 24.9:
         return "Normal"
-    elif 25<bmi<29.9:
+    elif 25 < bmi < 29.9:
         return "overweight"
     else:
         return "obseity"
 
+
 print(bmi)
-print(calculate_bmi(weight,height))
+print(calculate_bmi1(weight, height))
