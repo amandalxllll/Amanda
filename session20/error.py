e5154133b7d11041a1961e82f36573e742f8dda7
amandalxllll <xliu7@babson.edu>
2023-04-10 20:51:05 -0400

    h

--- session20/error.py
+++ session20/error.py
@@ -1,5 +1,16 @@
-number = float(input('please enter a number:>>'))
+try:
+    number = float(input('please enter a number:>>'))
 
-result = 2023/number
+    result = 2023/number
 
-print(result)
+    print(result)
+except ZeroDivisionError:
+    print ('You just entered 0')
+except ValueError:
+    print ("you should enter an integer")
+except:
+    print("something wrong happened")
+finally:
+    print("No matter what happens, we still got here")
+
+print("Hello, world!") # we still want to see this no matter what happen
